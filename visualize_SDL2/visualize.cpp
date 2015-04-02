46339088e7025c8d24ba5de306ffed1342857bf7
Richard Kirchofer <rgkirch@gmail.com>
2015-04-01 17:36:04 -0700

    make not working, says no sdl.h

--- visualize_SDL2/visualize.cpp
+++ visualize_SDL2/visualize.cpp
@@ -7,25 +7,24 @@
 #include <SDL_opengl.h>
 #include <GL/glu.h>
 
-#define WINDOW_WIDTH 1
-#define WINDOW_HEIGHT 1
+#define WINDOW_SIZE 1
 
 
-struct particle_t { float x, y; };
+struct particle_t
+{
+	float x, y;
+};
 	
 double read_timer( )
 {
     static bool initialized = false;
     static struct timeval start;
     struct timeval end;
-    if( !initialized )
-    {
+    if( !initialized ) {
         gettimeofday( &start, NULL );
         initialized = true;
     }
-	
     gettimeofday( &end, NULL );
-	
     return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
 }
 
@@ -37,11 +36,9 @@ void make_surface()
     glEnable( GL_BLEND );
 	// something about blending the colors coming in with the colors already there
     glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
-	
     glPointSize( 2 );
     glClearColor( 1, 1, 1, 1 );	
-   
-    glViewport (0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glViewport (0, 0, WINDOW_SIZE, WINDOW_SIZE);
 	// specifies which matrix stack is the target for subsequent matrix operations
 	// projection - apply subsequent operations to the matrix stack
     glMatrixMode(GL_PROJECTION);
@@ -52,17 +49,14 @@ void make_surface()
 int main( int argc, char *argv[] )
 {
     const char *filename = argc > 1 ? argv[1] : DEFAULT_FILENAME;
-	
     FILE *f = fopen( filename, "r" );
     if( f == NULL )
     {
         printf( "failed to find %s\n", filename );
         return 1;
     }
-    
     int n;
-    float size;
-    fscanf( f, "%d%g", &n, &size );
+    fscanf( f, "%d", &n );
 	
     particle_t p;
     std::vector<particle_t> particles;
@@ -70,29 +64,25 @@ int main( int argc, char *argv[] )
         particles.push_back( p );
     fclose( f );
 	
-    int nframes = particles.size( ) / n;
-    if( nframes == 0 )
-        return 2;
+    // int nframes = particles.size( ) / n;
+    // if( nframes == 0 )
+        // return 2;
     
-    int window_size = (int)((size+2*eps)*SCALE);
-    window_size = window_size > MIN_SIZE ? window_size : MIN_SIZE;
+    // int window_size = (int)((size+2*eps)*SCALE);
+    // window_size = window_size > MIN_SIZE ? window_size : MIN_SIZE;
 	
-    //
     // init graphics system
-    //
-    if( SDL_Init( SDL_INIT_VIDEO ) != 0 )
-    {
+    if( SDL_Init( SDL_INIT_VIDEO ) != 0 ) {
         printf( "SDL_Init failed: %s\n", SDL_GetError( ) );
         return 1;
     }
-	SDL_Window *window =  SDL_CreateWindow( "CS267 Particle Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, window_size, window_size, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE );
-    if( window == NULL )
-    {
+	SDL_Window *window =  SDL_CreateWindow( "Particle Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, window_size, window_size, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE );
+    if( window == NULL ) {
         printf( "SDL_SetVideoMode failed: %s\n", SDL_GetError( ) );
         exit( 1 );
     }
     SDL_GLContext glcontext = SDL_GL_CreateContext(window);
-    make_surface( window_size, window_size );
+    make_surface( WINDOW_SIZE, WINDOW_SIZE );
     for( bool done = false; !done; )
     {
         SDL_Event event;
@@ -108,7 +98,7 @@ int main( int argc, char *argv[] )
 		
         glMatrixMode( GL_PROJECTION );
         glLoadIdentity( );
-        gluOrtho2D( -eps, size+eps, -eps, size+eps );
+        gluOrtho2D( 0, WINDOW_SIZE, 0, WINDOW_SIZE );
 		
         glColor3f( 0.75, 0.75, 0.75 );
         glBegin( GL_LINE_LOOP );
@@ -122,8 +112,13 @@ int main( int argc, char *argv[] )
         particle_t *p = &particles[iframe*n];
 		
         glColor3f( 0, 0, 0 );
+		// defines how it should treat the following data, up to glEnd()
+		// GL_POINTS - treats each verted as a single point
         glBegin( GL_POINTS );
         for( int i = 0; i < n; i++ )
+			// glVertex2fv() specifies a vertex
+			// accepts a pointer to an array of two elemnts
+			// p is a struct but the elements are contiguous regardless
             glVertex2fv( &p[i].x );
         glEnd( );
 		
