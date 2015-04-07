287182e9fb9ffa5369d62fca8fdeda238e9b8fe7
Richard Kirchofer <rgkirch@gmail.com>
2015-04-07 05:12:10 -0700

    i dont see anything

--- gravity/mpi-gravity.cpp
+++ gravity/mpi-gravity.cpp
@@ -1,24 +1,119 @@
 #include <iostream>
+#include <stdio.h>
 #include <vector>
 #include <mpi.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <assert.h>
+#include <time.h>
+#include <sys/time.h>
+#include <math.h>
 #include <SFML/Graphics.hpp>
-#include "common-gravity.h"
 
 using namespace std;
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
+#define NSTEPS 500
+#define SAVEFREQ 1
 
+#define density	0.0005
+#define mass	1.0
+#define dt		0.0005
+
+double size = 0.0;
+
+inline int min( int a, int b ) { return a < b ? a : b; }
+inline int max( int a, int b ) { return a > b ? a : b; }
+
+typedef struct 
+{
+  double x;
+  double y;
+  double vx;
+  double vy;
+  double ax;
+  double ay;
+} particle_t;
+
+double read_timer( )
+{
+	static bool initialized = false;
+	static struct timeval start;
+	struct timeval end;
+	if( !initialized )
+	{
+		gettimeofday( &start, NULL );
+		initialized = true;
+	}
+	gettimeofday( &end, NULL );
+	return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
+}
+
+int find_option( int argc, char **argv, const char *option )
+{
+	for( int i = 1; i < argc; i++ )
+		if( strcmp( argv[i], option ) == 0 )
+			return i;
+	return -1;
+}
+
+int read_int( int argc, char **argv, const char *option, int default_value )
+{
+	int iplace = find_option( argc, argv, option );
+	if( iplace >= 0 && iplace < argc-1 )
+		return atoi( argv[iplace+1] );
+	return default_value;
+}
+
+double set_size( int n ) 
+{
+    size = sqrt( density * n );
+	return size;
+}
+
+void init_particles( int n, particle_t *p )
+{
+	srand( time( NULL ) );
+	for( int i = 0; i < n; i++ ) 
+	{
+		p[i].vx = rand()*10;
+		p[i].vy = rand()*10;
+		p[i].x = rand()*size;
+		p[i].y = rand()*size;
+	}
+}
+
+void apply_force( particle_t &particle, particle_t &neighbor)
+{
+	double xdiff = neighbor.x - particle.x;
+	double ydiff = neighbor.y - particle.y;
+	double r2 = xdiff * xdiff + ydiff * ydiff;
+	double force = 0;
+	if (r2 != 0)
+	{
+		force = 1/r2;
+	}
+	double direction = atan2(xdiff, ydiff);
+	particle.vx += sin(direction)*dt;
+	particle.vy += cos(direction)*dt;
+}
+
+void move( particle_t &p )
+{
+	p.vx += p.ax * dt;
+	p.vy += p.ay * dt;
+	p.x  += p.vx * dt;
+	p.y  += p.vy * dt;
+}
 
 void create_window(sf::RenderWindow &window)
 {
 	// create window
 	window.create( sf::VideoMode( WINDOW_WIDTH, WINDOW_HEIGHT ), "SFML Visualizer!" );
-	// window.setVerticalSyncEnabled( true );
-	// window.setFramerateLimit( 15 );
+	window.setVerticalSyncEnabled( true );
+	window.setFramerateLimit( 15 );
 }
 
 int main( int argc, char **argv )
@@ -49,7 +144,7 @@ int main( int argc, char **argv )
 	int nlocal = partition_sizes[rank];
 	particle_t *local = (particle_t*) malloc( nlocal * sizeof(particle_t) );
 	//	initialize
-	double size = set_size( n );
+	size = set_size( n );
 	// cout << "size:" << size << endl;
 	//	every process creates a pointer to a renderwindow
 	sf::RenderWindow window;
@@ -67,8 +162,8 @@ int main( int argc, char **argv )
 	MPI_Scatterv( particles, partition_sizes, partition_offsets, PARTICLE, local, nlocal, PARTICLE, 0, MPI_COMM_WORLD );
 	//	simulate a number of time steps
 	double simulation_time = read_timer( );
-	// for( int step = 0; step < NSTEPS; step++ )
-	while( 1 )
+	for( int step = 0; step < NSTEPS; step++ )
+	// while( 1 )
 	{
 		//	collect all global data locally
 		MPI_Allgatherv( local, nlocal, PARTICLE, particles, partition_sizes, partition_offsets, PARTICLE, MPI_COMM_WORLD );
