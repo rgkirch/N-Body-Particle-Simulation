77859b8ab15c020b27d795c7f08011acbadf1d68
Richie Kirchofer <rgkirch@gmail.com>
2015-04-07 17:34:41 -0400

    it worked before this, but not now, some are stuck

--- gravity/mpi-gravity.cpp
+++ gravity/mpi-gravity.cpp
@@ -1,12 +1,15 @@
+// Richard Kirchofer
 #include <assert.h>
 // has RAND_MAX
 #include <cstdlib>
+#include <cmath>
 #include <iostream>
 #include <math.h>
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <string>
 #include <sys/time.h>
 #include <time.h>
 #include <vector>
@@ -15,15 +18,21 @@
 
 using namespace std;
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
 #define NSTEPS 500
 #define SAVEFREQ 1
 
+#define massToDrawRadius	1.0
+#define defaultRadius	4.0
+#define cutoff	0.00001
 #define density	0.0005
-#define mass	1.0
+#define iniVel	10
+#define velFric	0.99
+// #define dt		0.0005
 #define dt		0.0005
-#define size 1.0
+// deprecated
+#define size	1.0
 
 // double size = 0.0;
 
@@ -32,12 +41,15 @@ inline int max( int a, int b ) { return a > b ? a : b; }
 
 typedef struct 
 {
-  double x;
-  double y;
-  double vx;
-  double vy;
-  double ax;
-  double ay;
+	double x;
+	double y;
+	double vx;
+	double vy;
+	double ax;
+	double ay;
+	double mass;
+	double displaySize;
+	sf::Color color;
 } particle_t;
 
 double read_timer( )
@@ -73,7 +85,7 @@ int read_int( int argc, char **argv, const char *option, int default_value )
 /*
 void set_size( int n ) 
 {
-    size = sqrt( density * n );
+	size = sqrt( density * n );
 }
 */
 
@@ -87,32 +99,54 @@ void init_particles( int n, particle_t *p )
 		p[i].vy = 0.0;
 		p[i].ax = 0.0;
 		p[i].ay = 0.0;
-		// p[i].vx = rand()*1;
-		// p[i].vy = rand()*1;
+		p[i].mass = 1.0;
+		p[i].displaySize = p[i].mass * 4.0;
+		p[i].vx = rand() / (double)RAND_MAX * iniVel;
+		p[i].vy = rand() / (double)RAND_MAX * iniVel;
 		p[i].x = rand() / (double)RAND_MAX;
 		p[i].y = rand() / (double)RAND_MAX;
+		p[i].color = sf::Color::Black;
 	}
 }
 
 void apply_force( particle_t &particle, particle_t &neighbor)
 {
-	double xdiff = neighbor.x - particle.x;
-	double ydiff = neighbor.y - particle.y;
-	double r2 = xdiff * xdiff + ydiff * ydiff;
-	double force = 0;
-	if (r2 != 0)
-	{
+	double dx = neighbor.x - particle.x;
+	double dy = neighbor.y - particle.y;
+	double r2 = dx * dx + dy * dy;
+	if( r2 == 0 ) {
+		return;
+	} else if( r2 < cutoff ) {
+		/*
+		double r = sqrt( r2 );
+		double coef = ( 1 - cutoff / r ) / r2 / mass;
+		particle.ax += coef * dx;
+		particle.ay += coef * dy;
+		*/
+
+		//double xdiff = (double)abs(particle.vx - neighbor.vx);
+		//double ydiff = (double)abs(particle.vy - neighbor.vy);
+		double xsum = (particle.vx + neighbor.vx);
+		double ysum = (particle.vy + neighbor.vy);
+		double xavg = xsum / 2;
+		double yavg = ysum / 2;
+		particle.vx = xavg;
+		particle.vy = yavg;
+	} else {
+		double force = 0.0;
 		force = 1/r2;
+		double direction = atan2(dx, dy);
+		particle.vx += sin(direction)*dt;
+		particle.vy += cos(direction)*dt;
 	}
-	double direction = atan2(xdiff, ydiff);
-	particle.vx += sin(direction)*dt;
-	particle.vy += cos(direction)*dt;
 }
 
 void move( particle_t &p )
 {
 	p.vx += p.ax * dt;
 	p.vy += p.ay * dt;
+	p.vx *= velFric;
+	p.vy *= velFric;
 	p.x  += p.vx * dt;
 	p.y  += p.vy * dt;
 }
@@ -134,7 +168,8 @@ int main( int argc, char **argv )
 	MPI_Comm_size( MPI_COMM_WORLD, &n_proc );
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 	//	allocate memory for particles
-	particle_t *particles = (particle_t*) malloc( n * sizeof(particle_t) );
+	// particle_t *particles = (particle_t*) malloc( n * sizeof(particle_t) );
+	particle_t *particles = new particle_t[n];
 	//	define a particle
 	MPI_Datatype PARTICLE;
 	MPI_Type_contiguous( 6, MPI_DOUBLE, &PARTICLE );
@@ -157,7 +192,7 @@ int main( int argc, char **argv )
 	// set_size( n );
 	//	every process creates a pointer to a renderwindow
 	sf::RenderWindow window;
-	//	define a circle
+	// create a circle object, must be visible to all proc
 	sf::CircleShape circle(4, 8);
 	circle.setFillColor( sf::Color::Black );
 	if( rank == 0 )
@@ -166,10 +201,16 @@ int main( int argc, char **argv )
 		//	init the particles
 		init_particles( n, particles );
 		//	only one process creates a window
+		// function call defined above
 		create_window(window);
 	}
 	//	distribute the particles
 	MPI_Scatterv( particles, partition_sizes, partition_offsets, PARTICLE, local, nlocal, PARTICLE, 0, MPI_COMM_WORLD );
+	// color proc 0 particles blue
+	for( int i = 0; i < nlocal; ++i)
+	{
+		local[i].color = sf::Color::Blue;
+	}
 	//	simulate a number of time steps
 	double simulation_time = read_timer( );
 	// for( int step = 0; step < NSTEPS; step++ )
@@ -180,13 +221,18 @@ int main( int argc, char **argv )
 		if( rank == 0 )
 		{
 			window.clear( sf::Color::White );
-			sf::CircleShape temp = circle;
 			for( int i = 0; i < n; ++i )
 			{
-				temp.setPosition( particles[i].x / size * WINDOW_WIDTH, particles[i].y / size * WINDOW_HEIGHT );
+				// scale the default radius by the mass
+				// circle.setRadius(defaultRadius * massToDrawRadius * particles[i].mass);
+				circle.setRadius(particles[i].displaySize);
+				circle.setFillColor(particles[i].color);
+				// fmod is floating point modulus
+				// don't do modulus TODO
+				circle.setPosition( fmod( particles[i].x / size * WINDOW_WIDTH, WINDOW_WIDTH), fmod(particles[i].y / size * WINDOW_HEIGHT, WINDOW_HEIGHT ) );
 				// cout << particles[i].x << " " << particles[i].y << endl;
-				// temp.setPosition( .5 * WINDOW_WIDTH, .5 * WINDOW_HEIGHT );
-				window.draw( temp );
+				// circle.setPosition( .5 * WINDOW_WIDTH, .5 * WINDOW_HEIGHT );
+				window.draw( circle );
 			}
 			window.display();
 		}
@@ -212,7 +258,8 @@ int main( int argc, char **argv )
 	free( partition_offsets );
 	free( partition_sizes );
 	free( local );
-	free( particles );
+	// free( particles );
+	delete particles;
 	//	end mpi portion
 	MPI_Finalize( );
 	return 0;
