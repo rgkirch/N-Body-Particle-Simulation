a9c393436f85b0c541a572c70f217e0663ebbea9
Richie Kirchofer <rgkirch@gmail.com>
2015-04-29 16:07:25 -0400

    Report 'finished' and submitted.

--- report/latex/RichardKirchofer.d
+++ report/latex/RichardKirchofer.d
@@ -1,3 +1,5 @@
 # vim: ft=make
 .PHONY: RichardKirchofer._graphics
+RichardKirchofer.aux RichardKirchofer.aux.make RichardKirchofer.d RichardKirchofer.pdf: $(call path-norm,/usr/share/texlive/texmf-dist/tex/latex/base/article.cls)
+RichardKirchofer.aux RichardKirchofer.aux.make RichardKirchofer.d RichardKirchofer.pdf: $(call path-norm,RichardKirchofer.tex)
 .SECONDEXPANSION:
